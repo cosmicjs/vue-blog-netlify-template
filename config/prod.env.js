915c2337d80c949ea8c02c6006ec0d03e0e2ddef
Jazib Sawar <jazibsawar@gmail.com>
2018-08-08 20:50:20 +0500

    Configuration done right

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   COSMIC_BUCKET: `"${process.env.COSMIC_BUCKET || 'simple-vue-blog'}"`,
-  COSMIC_READ_KEY: `"${process.env.COSMIC_READ_KEY || 'ciKXDCOucTue902gHdfigrI8KN6IjoCeZtVpvhiXhZ7KGcOFiq'}"`,
-  COSMIC_WRITE_KEY: `"${process.env.COSMIC_WRITE_KEY || 'YWSOdRn0LChwDXzkGrkoGX8o9TPyukVxLP5QhgKMpW6UHhGTBk'}"`,
+  COSMIC_READ_KEY: `"${process.env.COSMIC_READ_KEY}"`,
+  COSMIC_WRITE_KEY: `"${process.env.COSMIC_WRITE_KEY}"`,
   BLOG_TITLE: `"${process.env.BLOG_TITLE || 'Vue Blog Cosmic JS'}"`,
 }
